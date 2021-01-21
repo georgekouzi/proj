e0d7989c77f89e8a221d7f61cd92aa657ae0b7bf
George <57435555+georgekouzi@users.noreply.github.com>
2021-01-21 16:02:48 +0200

    cdcd

---
+++ test1/src/test1/dolev.java
@@ -0,0 +1,9 @@
+package test1;
+
+public class dolev {
+
+	public static void main(String[] args) {
+		System.out.println("dolev");
+	}
+
+}
---
+++ test1/src/test1/george.java
@@ -0,0 +1,12 @@
+package test1;
+
+public class george {
+
+	public static void main(String[] args) {
+
+		
+		System.out.println("george");
+		
+	}
+
+}
