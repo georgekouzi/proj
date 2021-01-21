e6613cd8ca15e03641ebf978f84d6516fb4cc803
George <57435555+georgekouzi@users.noreply.github.com>
2021-01-21 16:11:37 +0200

    dscd

--- test1/src/test1/dolev.java
+++ test1/src/test1/dolev.java
@@ -1,14 +1,47 @@
 package test1;
 
+
 public class dolev {
 
+	//אני לא מצליח את הסעיף השני בשאלה אחת
 	
 	
+	//שאלה 1 סעיף א
 	
 	
-	
-	
-	
+	public static String TheCheapestPath(Node[][] mat) {//o(n+m)*num_of_cheapest_path
+//		cleanValue();
+		int i=mat.length-1;
+		int j=mat[0].length-1;
+		String path ="";
+
+		while(i>0&&j>0) {
+			int left=mat[i][j-1].value+mat[i][j-1].right;
+			int up= mat[i-1][j].value+mat[i-1][j].down; 
+			if(left<up) {
+				path="R"+path;
+				j--;
+			}
+			else { // up<=right
+				path="D"+path;
+				i--;
+			}
+		}
+
+		while(j>0) {
+			path="R"+path;
+			j--;
+
+		}
+		while(i>0) {
+
+			path="D"+path;
+			i--;
+
+		}
+
+		return path;
+	}
 	
 	
 	
